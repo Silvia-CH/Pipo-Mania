c22b555f434c946106a107f3e0bdff72de941858
Silvia <129101122+Silvia-CH@users.noreply.github.com>
2024-03-20 12:13:27 +0100

    First Commit

---
+++ hud.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+signal pause
+signal unpause
+
+var is_paused = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_E and is_paused:
+			$TextEdit.hide()
+			$ColorRect.hide()
+			unpause.emit()
+			is_paused = false
+		elif event.keycode == KEY_E and not is_paused:
+			$TextEdit.show()
+			$ColorRect.show()
+			pause.emit()
+			is_paused = true
+			
+func _on_enter_pressed():
+	$Show.text = $TextEdit.text
+
+
+func _on_clear_pressed():
+	$Show.text = "" 
+	$TextEdit.text = ""
+	
