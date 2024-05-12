562aabb628bb20177f7baf6304c4729288f230ea
Yutopya <124583415+Yutopya@users.noreply.github.com>
2024-05-12 22:57:28 +0200

    login menu fix

--- interfaces/PuaseUI.gd
+++ interfaces/PuaseUI.gd
@@ -1,7 +1,8 @@
 extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 @onready var salir = $ContenedorPadre/MarginContainer/VBoxContainer/HBoxContainer/Salir
-
+@onready var login = $"ContenedorPadre/MarginContainer/VBoxContainer/Ventanas/Inicio de sesion/LoginUI"
+@onready var visibilidad = false;
 signal salir_menu_opciones
 
 func _ready():
@@ -10,3 +11,12 @@ func _ready():
 func _on_salir_pressed():
 	salir_menu_opciones.emit()
 	set_process(false)
+
+
+func _on_ventanas_tab_clicked(tab):
+	if(tab==1 && !visibilidad):
+		login.visible=true
+		visibilidad=true
+	else: 
+		login.visible=false
+		visibilidad=false
