6cfb4de5cbdd3e19f9ba314d9103be32d1e71511
Yutopya <Yuri.maximino.navarro@gmail.com>
2024-06-03 23:29:53 +0200

    UI Overhaul 3 Separating UI's

--- interfaces/MainUI.gd
+++ interfaces/MainUI.gd
@@ -1,26 +1,31 @@
 extends Control
 
-@onready var button_1 = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Button
-@onready var button_2 = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Button2
-@onready var button_3 = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Button3
 @export var partida_precargada = preload("res://main.tscn")
 @export var menu_Opciones_precargada = preload("res://interfaces/PuaseUI.tscn")
+@export var menu_Cuenta_precargada = preload("res://interfaces/AccountUI.tscn")
 
-func _on_button_button_down():
+func _on_empezar_button_down():
 	get_tree().change_scene_to_packed(partida_precargada)
 
+func _on_cuenta_button_down():
+	$MarginContainer.visible=false
+	$AccountUi.set_process(true)
+	$AccountUi.visible=true
+	
+func _on_account_ui_salir_menu_cuenta():
+	$MarginContainer.visible=true
+	$AccountUi.visible=false
 
-func _on_button_2_button_down():
+func _on_opciones_button_down():
 	$MarginContainer.visible=false
 	$PuaseUi.set_process(true)
 	$PuaseUi.visible=true
-	
-
-
-func _on_button_3_button_down():
-	get_tree().quit()
-
 
 func _on_puase_ui_salir_menu_opciones():
 	$MarginContainer.visible=true
 	$PuaseUi.visible=false
+
+func _on_salir_button_down():
+	get_tree().quit()
+
+
