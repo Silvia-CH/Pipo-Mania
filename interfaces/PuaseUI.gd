7a48b09451996c051fbb8537f8b39fa5dc373f29
Yutopya <Yuri.maximino.navarro@gmail.com>
2024-06-13 16:50:55 +0200

    Clasification v1

--- interfaces/PuaseUI.gd
+++ interfaces/PuaseUI.gd
@@ -6,6 +6,8 @@ signal salir_menu_opciones
 signal volver_menu_principal
 var is_paused = false
 
+## Funcion a la espera de inputs
+## Activa la vista de opciones al presionar ESC
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_ESCAPE and not is_paused:
@@ -17,6 +19,7 @@ func _input(event):
 			is_paused = false
 			$"../PuaseUi".visible = false
 
+## Vuelve al menu principal al presionar el boton
 func _on_volver_al_menu_pressed():
 	unpause.emit()
 	volver_menu_principal.emit()
